079c5e935bd378e776f53f7fe2ce815528fdf23b
abyssresearcher1 <akylbekashyralie@gmail.com>
2024-10-10 16:26:41 +0600

    added footer sample

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <Header children={undefined} />
       {children}
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import footerLogo from "../../assets/FooterLogo.png";
+import certificate from "../../assets/sertificate.png";
+import facebook from "../../assets/Facebook .svg";
+import twitter from "../../assets/Twitter.svg";
+import insta from "../../assets/instagram.svg";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="Footer">
+      <div className="footer-general">
+        <div className="footer-logo">
+          <img src={footerLogo} alt="Footer-logo" />
+        </div>
+        <div className="footer-nav">
+          <div className="Links footer-links">
+            <h3>Quick links</h3>
+            <a href="#">About us</a>
+            <a href="#">Bulk order</a>
+            <a href="#">Gifts</a>
+            <a href="#">Organic Garden</a>
+          </div>
+          <div className="Legal footer-links">
+            <h3>Legal</h3>
+            <a href="#">T&C</a>
+            <a href="#">Privacy policy</a>
+            <a href="#">Returns</a>
+            <a href="#">Shipping</a>
+            <a href="#">Cancellation</a>
+          </div>
+          <div className="Support footer-links">
+            <h3>Support</h3>
+            <a href="#">FAQs</a>
+            <a href="#">Contact us</a>
+          </div>
+        </div>
+        <div className="footer-license">
+          <img src={certificate} alt="certificate" />
+        </div>
+        <div className="footer-contacts">
+          <div className="contacts-text">
+            © 2021 Plan A Plant All Rights Reserved
+          </div>
+          <div className="footer-icons">
+            <img src={facebook} alt="" />
+            <img src={twitter} alt="" />
+            <img src={insta} alt="" />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
